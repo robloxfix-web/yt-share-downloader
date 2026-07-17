e32a0386e04754eaebc4c46b3576e1b89ecf6008
robloxfix <rycrhc14@gmail.com>
2026-07-17 20:13:17 +0100

    Remove ProGuard and YouTube Download Library dependencies
    Removed ProGuard files configuration and YouTube Download Library dependencies.

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,10 +18,6 @@ android {
     buildTypes {
         release {
             isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
         }
     }
 
@@ -29,7 +25,6 @@ android {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
     }
-
     kotlinOptions {
         jvmTarget = "1.8"
     }
@@ -39,8 +34,4 @@ dependencies {
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
-
-    // YouTube Download Library (النسخة الصحيحة)
-    implementation("com.github.yausername.youtubedl-android:library:0.17.0")
-    implementation("com.github.yausername.youtubedl-android:ffmpeg:0.17.0")
 }
