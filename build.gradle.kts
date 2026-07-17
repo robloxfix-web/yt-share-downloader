3105ddf493ed1e5636ba85e7efc97c14ecf0c808
robloxfix <rycrhc14@gmail.com>
2026-07-17 19:53:29 +0100

    Configure Android application in build.gradle.kts
    Added Android configuration with compile and target SDK versions, default config, build types, and Kotlin options.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,36 @@
 plugins {
-    id("com.android.application") version "8.2.0" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.10" apply false
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+}
+
+android {
+    namespace = "com.ytshare.app"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.ytshare.app"
+        minSdk = 24
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
 }
